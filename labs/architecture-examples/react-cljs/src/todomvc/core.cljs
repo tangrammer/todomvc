388cb2e963b02bd606d8c87e53605eec1af1e404
David Nolen <david.nolen@gmail.com>
2013-12-12 10:30:19 -0500

    wip

--- labs/architecture-examples/react-cljs/src/todomvc/core.cljs
+++ labs/architecture-examples/react-cljs/src/todomvc/core.cljs
@@ -7,22 +7,43 @@
 
 (def app-state (atom {:todos []}))
 
-(defn edit-todo-item [item]
+;; =============================================================================
+;; Todo Item
+
+(defn edit-todo [e m])
+
+(defn delete-todo [e m])
+
+(defn toggle-todo [e m])
+
+(defn save-todo [e m]
   )
 
-(defn todo-item [handle-key]
-  )
+(defn todo-item [{:keys [completed editing] :as todo} path]
+  (dom/li #js {:className (str (and completed "completed") " "
+                              (and editing "editing"))}
+    (dom/div #js {:className "view"}
+      (dom/input #js {:className "toggle"
+                      :type "checkbox"
+                      :checked (and completed "checked")
+                      :onChange (om/bind toggle-todo path)})
+      (dom/label #js {:onDoubleClick (om/bind edit-todo path)}
+        (:title todo))
+      (dom/button #js {:className "destroy" :onClick (om/bind delte-todo path)})
+      (dom/input #js {:ref "editField"
+                      :className "edit"
+                      :value (:text todo)
+                      :onBlur (om/bind save-todo path)
+                      :onChange (om/bind handle-todo-change path)
+                      :onKeyDown (om/bind handle-todo-key-down path)}))))
 
-(defn todo-item [todo]
-  )
+;; =============================================================================
+;; Todos
 
 (defn toggle-all [e]
   (let [checked (.. e -target -checked)]
     (swap! app-state update-in [:todos]
       (fn [todos] (map (fn [todo] (assoc todo :completed checked)) todos)))))
-
-(defn toggle [todo]
-  )
 
 (defn handle-new-todo-keydown [e {:keys [state path owner]}]
   (when (not (identical? (.-which e) ENTER_KEY))
@@ -37,7 +58,9 @@
                     :type "checkbox"
                     :onChange toggle-all})
     (dom/ul #js {:id "todo-list"}
-      (into-array (map todo-item todos)))))
+      (into-array
+        (map #(om/render todo-item todos path %)
+          todos (range (count todos)))))))
 
 (defn footer [data path]
   (dom/footer #js {:id "footer"}
@@ -61,13 +84,12 @@
     (fn [data path]
       (dom/div nil
         (dom/header #js {:id "header"}
-          (array
-            (dom/h1 nil "todos")
-            (om/bind dom/input
-              #js {:ref "newField"
+          #js [(dom/h1 nil "todos")
+               (om/bind dom/input
+               #js {:ref "newField"
                     :id "new-todo"
                     :placeholder "What needs to be done?"
                     :onKeyDown (om/bind handle-new-todo-keydown path)})
-            (om/render main data (conj path :todos))
-            (om/render footer data path)))))
+               (om/render main data path :todos)
+               (om/render footer data path)])))
     js/document.body))
