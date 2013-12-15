3b5d74bc586468368cddfa8b1ec6478afd75bf11
David Nolen <david.nolen@gmail.com>
2013-12-14 19:49:39 -0500

    add store to utils

--- labs/architecture-examples/om/src/todomvc/utils.cljs
+++ labs/architecture-examples/om/src/todomvc/utils.cljs
@@ -1,4 +1,5 @@
 (ns todomvc.utils
+  (:require [clojure.reader :as reader])
   (:import [goog.ui IdGenerator]))
 
 (defn guid []
@@ -11,3 +12,13 @@
   (if (== n 1)
     word
     (str word "s")))
+
+(defn store
+  ([ns] (store ns nil))
+  ([ns edn]
+    (if-not (nil? edn)
+      (.setItem js/localStorage (str edn))
+      (let [s (.getItem js/localStorage ns)]
+        (if-not (nil? s)
+          (reader/read-string s)
+          [])))))
