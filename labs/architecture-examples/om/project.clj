6c27c2fdc3ae8c955ac6cfa09fa145bafe0dc8f8
David Nolen <david.nolen@gmail.com>
2013-12-18 14:06:33 -0500

    bump version

--- labs/architecture-examples/om/project.clj
+++ labs/architecture-examples/om/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2125"]
+                 [org.clojure/clojurescript "0.0-2127"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [secretary "0.4.0"]
                  [om "0.1.0-SNAPSHOT"]]
